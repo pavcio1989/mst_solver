ccbe8d23f166c369b0fbd4e642fb823b6510dfda
pawel <pavcio1989@gmail.com>
2025-04-11 16:28:52 +0200

    first prim algo changes #2

--- src/solvers/base.py
+++ src/solvers/base.py
@@ -7,11 +7,17 @@ from src.entities.edges import Edge
 
 
 class MinimumSpanningTreeBaseSolver:
+    number_of_edges = None
     solved = False
+    mst_exists = False
     graph = defaultdict(list)
+    # Outputs
+    mst_min_cost = None
+    mst_edges = []
 
     def __init__(self, config: Config):
         self.n = config.node_count
+        self.visited = self.n * [False]
         self.s = config.source_id
         self.t = config.sink_id
         self.config = config
@@ -25,12 +31,27 @@ class MinimumSpanningTreeBaseSolver:
         for edge_raw in edges_raw:
             self.add_edge(edge_raw[0], edge_raw[1], edge_raw[2])
 
-    def add_edge(self, start_node, end_node, capacity):
-        if capacity <= 0:
+    def add_edge(self, start_node, end_node, cost):
+        if cost <= 0:
             raise Exception("Forward edge capacity <=0")
-        edge1 = Edge(start_node, end_node, capacity)
+        edge1 = Edge(start_node, end_node, cost)
 
         self.graph[start_node].append(edge1)
 
+    def get_mst(self):
+        self.solve()
+        if self.mst_exists:
+            return self.mst_edges
+        else:
+            return None
+
     def get_mst_cost(self):
-        pass
+        self.solve()
+        if self.mst_exists:
+            return self.mst_min_cost
+        else:
+            return None
+
+    def if_mst_exists(self):
+        self.solve()
+        return self.mst_exists
--- src/solvers/prim.py
+++ src/solvers/prim.py
@@ -1,5 +1,34 @@
+from collections import deque
+
 from src.solvers.base import MinimumSpanningTreeBaseSolver
 
 
+MAX_COST = 1000000
+
 class PrimMstSolver(MinimumSpanningTreeBaseSolver):
-    pass
+    def __init__(self, config):
+        super().__init__(config)
+
+    def solve(self):
+        if self.solved:
+            return
+        self.solved = True
+
+        m = self.n - 1
+        edge_count = 0
+        visited = []
+        mst_edges = []
+
+        ipq = deque()
+
+    def relax_edges_at_node(self, current_node_index):
+        self.visited[current_node_index] = True
+
+        edges = self.graph.get(current_node_index)
+
+        for edge in edges:
+            dest_node_index = edge.end
+            # Skip edges pointing to already visited nodes
+            if self.visited[dest_node_index]:
+                continue
+
